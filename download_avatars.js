a79afe715b623e84aa44317fb4d5616f1a6003d3
Jorgelduarte <duarte.junior@gmail.com>
2018-02-19 19:55:00 +0000

    Adding Headers - secret token as .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,20 @@
 var request = require('request');
+var secrets = require("./secrets");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'Jorgelduarte',
+        "Authorization": 'token ' + secrets.GITHUB_TOKEN
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
   }
+  
 
   getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
