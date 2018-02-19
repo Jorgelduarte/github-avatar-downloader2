6a47381773ab593f6e63bbe4acfc29f85f8d12ef
Jorgelduarte <duarte.junior@gmail.com>
2018-02-19 22:12:01 +0000

    If condition was implanted for both arguments and to creat a folder if it doesnt exist.

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,57 @@
 var request = require('request');
 var secrets = require("./secrets");
+var fs = require('fs');
+var repositoryOwner = process.argv[2]; 
+var repositoryName = process.argv[3];
+
+if (!repositoryName || !repositoryOwner){
+    return console.log ("error message: you need insert both arguments") 
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-      headers: {
-        'User-Agent': 'Jorgelduarte',
-        "Authorization": 'token ' + secrets.GITHUB_TOKEN
-      }
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'jorgelduarte',
+            'Authorization': 'token ' + secrets.GITHUB_TOKEN
+        }
     };
-  
-    request(options, function(err, res, body) {
-      cb(err, body);
-    });
-  }
-  
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
+    request(options, function(err, res, body) {
+        cb(err, body);
+    });
+   
+}
+
+
+getRepoContributors(repositoryOwner, repositoryName, function(err, result) {
     console.log("Errors:", err);
     var resultJSON = JSON.parse(result);
-    for (var i = 0; i < resultJSON.length; i++){
-        console.log("Result:", resultJSON[i].avatar_url);
+
+    if (fs.existsSync("avatars")) {
+        console.log ("The folder 'avatars' alredy exist")
+    } else {
+        console.log ("The folder 'avatars' was created")
+        fs.mkdir('avatars',function(result){
+        })
     }
-  });
+    
+    for (var i = 0; i < resultJSON.length; i++){
+        var newUrl = resultJSON[i].avatar_url;
+        var login = resultJSON[i].login
+
+        function downloadImageByURL(onlyUrl, filePath) {
+            request.get(onlyUrl)              
+            .on('error', function (err) {                                   
+                throw err; 
+            })
+            .on('response', function (response) {
+                console.log('Download concluded with StatusCode: ', response.statusCode);
+            })
+            .pipe(fs.createWriteStream(filePath));
+                       
+        }
+        downloadImageByURL(newUrl, "avatars/" + login + ".jpg");
+    }
+});
+
